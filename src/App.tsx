df83e1a028acdfcfb3cc2b289099fec40c3e205a
ArieldRio <arieldelrioviamonte@gmail.com>
2021-10-06 16:10:38 -0400

    Add initial fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { domain } from "./config";
+import "./App.css";
+
+interface Product {
+  id: string;
+  no_almacen: number;
+  cod_producto: string;
+  descripcion: string;
+  parte_primaria?: string;
+  parte_2?: string;
+  parte_3?: string;
+  published_at: string;
+  createdAt: string;
+  updateAt: string;
+}
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`${domain}/productos`);
+        const data = await response.json();
+        setProducts(data);
+        setLoading(false);
+      } catch (error) {
+        console.log("error", error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading
+        ? "...loading"
+        : products.map((product) => (
+            <div key={product.id}>{product.cod_producto}</div>
+          ))}
     </div>
   );
 }
